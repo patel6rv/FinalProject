4b578aa276c3fc88b916a9be8b2aa301d298899a
patel6rv <patel6rv@mail.uc.edu>
2022-11-30 16:00:55 -0500

    ? added

--- FinalProject/Models/Book.cs
+++ FinalProject/Models/Book.cs
@@ -8,9 +8,9 @@ namespace FinalProject.Models
     public class Book
     {
         public int Id {get; set;}
-        public string Title {get; set;}
-        public string Author {get; set;}
-        public string Description {get; set;}
+        public string ?Title {get; set;}
+        public string ?Author {get; set;}
+        public string ?Description {get; set;}
         public int PublishYear {get; set;}
 
 
--- FinalProject/Models/ProgrammingLanguage.cs
+++ FinalProject/Models/ProgrammingLanguage.cs
@@ -8,10 +8,10 @@ namespace FinalProject.Models
     public class ProgrammingLanguage
     {
         public int Id {get; set;}
-        public string Name {get; set;}
-        public string Typed {get; set;}
-        public string Execution {get; set;}
-        public string Paradigm {get; set;}
+        public string ?Name {get; set;}
+        public string ?Typed {get; set;}
+        public string ?Execution {get; set;}
+        public string ?Paradigm {get; set;}
         
 
     }
